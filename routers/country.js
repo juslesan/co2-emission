2fe4e3a1f41a04538d1481b3f5ac1c83abae5b75
Juslesan <santeri.juslenius@gmail.com>
2019-01-12 13:09:22 +0200

    data to front

--- routers/country.js
+++ routers/country.js
@@ -6,20 +6,20 @@ const xml = require('xml');
 const jsonParser = require('./../utils/jsonParser')
 
 countryRouter.get('/:name', async (req, res) => {
-    const populations = await csvReader.readCSV('./external_apis/API_EN.ATM.CO2E.KT_DS2_en_csv_v2_10224872.csv')
+    const populations = await csvReader.readCSV('./external_apis/API_SP.POP.TOTL_DS2_en_csv_v2_10224786.csv')
+
     const index = jsonParser.jsonParseName(populations, req.params.name)
     // console.log(populations[2])
-    console.log(index)
+    // console.log(index)
 
     if (index > -1) {
-        const emissions = await csvReader.readCSV('./external_apis/API_SP.POP.TOTL_DS2_en_csv_v2_10224786.csv')
+        const emissions = await csvReader.readCSV('./external_apis/API_EN.ATM.CO2E.KT_DS2_en_csv_v2_10224872.csv')
         const infoJson = {
             name: populations[index]["Data Source"],
-            populations: populations[index],
-            emissions: emissions[index]
+            info: jsonParser.jsonYears(populations[index], emissions[index])
         }
         res.setHeader('Content-Type', 'application/json');
-
+        // console.log(infoJson)
         res.send(infoJson)
     }
     else {
--- utils/jsonParser.js
+++ utils/jsonParser.js
@@ -5,13 +5,30 @@ const jsonParseName = (json, name) => {
     
     for (let i = 0; i < count; i++) {
         if (json[i]['Data Source'] == name) {
-            console.log(i)
+            // console.log(i)
             return i;
         }
     }
     return -1;
 }
 
+
+const jsonYears = (populations, emissions) => {
+    let count = Object.keys(populations).length;
+
+    // console.log(Object.values(populations))
+    const countryStats = []
+    for (let i = 4; i < count; i++) {
+        countryStats.push({
+            population: Object.values(populations)[i],
+            emissions: Object.values(emissions)[i]
+        })
+    }
+    return countryStats
+} 
+
 module.exports = {
-    jsonParseName
+    jsonParseName,
+    jsonYears
+    
   }
